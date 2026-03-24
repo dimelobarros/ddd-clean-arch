c61ed8bd9496465aef65aeb7ae0753032969be07
Diego <diegodemelobarros@gmail.com>
2026-03-24 20:06:04 -0300

    Foram adicionadas as classes do diretório Endereco (InserirEndereco, RemoverEndereco, ListarEndereco, BuscarEnderecoPorId, AtualizarEndereco)

---
+++ src/modules/Endereco/Application/ListarEnderecos.ts
@@ -0,0 +1,9 @@
+import { EnderecoRepository } from "../Infrastructure/EnderecoRepository";
+
+export class ListarEnderecos{
+    constructor(private repository = new EnderecoRepository()){}
+
+    async execute(){
+        return this.repository.listarEnderecos;
+    }
+}
